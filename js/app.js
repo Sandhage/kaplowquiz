b01d8b001201d775ab13f3b56e34ddfbd44a9d7c
sandhage <kylesandhage@gmail.com>
2015-11-11 23:38:58 -0500

    Started creating question objects and generating random answer values.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // Global Variables
-defuseCode = null;
+var defuseCode  = null;
+var cutRed      = null;
+var cutGreen    = null;
+var cutBlue     = null;
+var cutYellow   = null;
+var ejectPower  = null;
 
 
 // Application
@@ -29,17 +34,38 @@ function startTimer(duration, display) {
 }
 
 window.onload = function() {
-    var fiveMinutes = 60 * 30;
+    // Set the timer for 30 seconds
+    var thirtySeconds = 60 * 30;
         display = document.querySelector('#timerDiv');
     document.querySelector('button').addEventListener('click', function () {
-    	startTimer(fiveMinutes, display);
+    	startTimer(thirtySeconds, display);
         showHints();
     });
+
+    // Set the 4-digit defusal code
+    setCode(0, 9999);
+
+    // Set Wires
+    setWires();
+
+    // Set Power Source
+    setPower();
+
+    // Set the question objects
+    var codeQuery = mkdefuseQuery("Four Digit Code", "What is the 4 digit code?", defuseCode);
+    console.log(codeQuery);
+    var wireQuery = mkdefuseQuery("Wires", "Which wires do you need to cut?", [cutRed, cutGreen, cutBlue, cutYellow]);
+    console.log(wireQuery);
+    var powerQuery = mkdefuseQuery("Power Source", "Do you need to remove the power source?", ejectPower);
+    console.log(powerQuery);
+
 };
 
 
 
 // Global Functions
+
+// Show/Hide functions
 function showHints() {
     document.getElementById('hintSect').style.display = "block";
 };
@@ -56,26 +82,84 @@ function hideQuestion() {
     document.getElementById('defuseSect').style.display = "none";
 };
 
+// Make "defusalQuery" (AKA Question) Objects
 function mkdefuseQuery(type, query, items) {
     var defuseQuery = {};
 
     defuseQuery.type    = type;
     defuseQuery.query   = query;
     defuseQuery.items   = items;
-    defuseQuery.guess   = null;
-    defuseQuery.correct = null;
+    defuseQuery.guess   = '';
 
-}
+    return defuseQuery;
 
-function setCode(min, max) {
-    defuseCode = Math.floor(Math.random()*(max-min+1)+min);
-    if (defuseCode < 1000) {
-        defuseCode = "0" + defuseCode;
-    };
-    console.log(defuseCode);
 };
 
+// 4-digit Defusal Code Generator
+function setCode(min, max) {
+    // Randomly generate a number between two digits ex: (0, 9999)
+    defuseCode = Math.floor(Math.random()*(max-min+1)+min);
 
+    // Else if chain in case number is less than 4 digits long
+    if (defuseCode < 10) {
+        defuseCode = "000" + defuseCode;
+    } else if (defuseCode < 100) {
+        defuseCode = "00" + defuseCode;
+    } else if (defuseCode > 100 && defuseCode < 1000) {
+        defuseCode = "0" + defuseCode;
+    };
+};
+
+// Wire Boolean Value Generator
+function setWires() {
+    // Set wire color variables, randomly generate values 1-10
+    cutRed    = Math.floor((Math.random() * 10) + 1);
+    cutGreen  = Math.floor((Math.random() * 10) + 1);
+    cutBlue   = Math.floor((Math.random() * 10) + 1);
+    cutYellow = Math.floor((Math.random() * 10) + 1);
+
+    // Translate cutRed to Boolean
+    if (cutRed <= 5) {
+        cutRed = true;
+    } else {
+        cutRed = false;
+    };
+
+    // Translate cutGreen to Boolean
+    if (cutGreen <= 5) {
+        cutGreen = true;
+    } else {
+        cutGreen = false;
+    };
+
+    // Translate cutBlue to Boolean
+    if (cutBlue <= 5) {
+        cutBlue = true;
+    } else {
+        cutBlue = false;
+    };
+
+    // Translate cutYellow to Boolean
+    if (cutYellow <= 5) {
+        cutYellow = true;
+    } else {
+        cutYellow = false;
+    };
+
+    // Check to make sure it's working!
+    // console.log(cutRed, cutGreen, cutBlue, cutYellow);
+};
+
+// Power Source Boolean Value Generator
+function setPower() {
+    ejectPower = Math.floor((Math.random() * 10) + 1);
+
+    if (ejectPower <= 5) {
+        ejectPower = true;
+    } else {
+        ejectPower = false;
+    };
+};
 
 /* Defunct Parts And Things That Worked But Weren't What I Needed
 
