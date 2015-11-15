60f925e80a5480d88a6cd35ac74af6fffedeee3f
sandhage <kylesandhage@gmail.com>
2015-11-15 18:04:19 -0500

    Set up questions/answers.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,18 @@ var codeQuery       = null;
 var wireQuery       = null;
 var powerQuery      = null;
 var currentQuestion = null;
+    // Test for functionality, make local later (maybe?)
+var words  = ["spooky", "tired", "gnarly", "mystery", "brave"];
+var words2 = ["ghost", "archer", "mister", "monkey", "sick"];
+var words3 = ["devil", "arrow", "explosion", "proceed", "sleeps"];
+    // Different Words
+var word1 = null;
+var word2 = null;
+var word3 = null;
+    // Phrase
+var passPhrase = null;
+    // Elements for class select and loop
+var elements = new Array();
 
 // Application //
 function startTimer(duration, display) {
@@ -36,6 +48,10 @@ function startTimer(duration, display) {
             setQuestion(powerQuery);
         }
 
+        if (minutes == 6 && seconds == 0) {
+            setQuestion(phraseQuery);
+        }
+
         // if (minutes == 6 && seconds == 0) {
         //     setQuestion();
         // }
@@ -50,6 +66,9 @@ function startTimer(duration, display) {
 }
 
 window.onload = function() {
+    // Set element class selector
+    elements = document.getElementsByClassName('answer');
+
     // Set all defusal variables (Code, Wire, Powersource)
     resetDefusal();
 
@@ -68,10 +87,6 @@ window.onload = function() {
         startTimer(thirtySeconds, display);
         showHints();
     });
-
-    // Pull questions to container -- will eventually be moved timer/answer
-
-
 };
 
 
@@ -81,19 +96,31 @@ window.onload = function() {
 // Show/hide functions
 function showHints() {
     document.getElementById('hintSect').style.display = "block";
-};
+}
 
 function hideHints() {
     document.getElementById('hintSect').style.display = "none";
-};
+}
 
 function showQuestion() {
     document.getElementById('defuseSect').style.display = "block";
-};
+}
 
 function hideQuestion() {
     document.getElementById('defuseSect').style.display = "none";
-};
+}
+
+function showAnswers() {
+    for (var i = 0, max = elements.length; i < max; i++) {
+        elements[i].style.display = "block";
+    };
+}
+
+function hideAnswers() {
+    for (var i = 0, max = elements.length; i < max; i++) {
+        elements[i].style.display = "none";
+    };
+}
 
 // Make "defusalQuery" (AKA Question) objects
 function mkdefuseQuery(type, query, items) {
@@ -107,17 +134,18 @@ function mkdefuseQuery(type, query, items) {
 
     return defuseQuery;
 
-};
+}
 
 // Set All "defusalQuery" (AKA Question) objects
 function setdefuseQueries() {
-    codeQuery = mkdefuseQuery("Four Digit Code", "What is the 4 digit code?", defuseCode);
+    codeQuery   = mkdefuseQuery("Four Digit Code", "What is the 4 digit code?", defuseCode);
         // console.log(codeQuery);
-    wireQuery = mkdefuseQuery("Wires", "Which wires do you need to cut?", [cutRed, cutGreen, cutBlue, cutYellow]);
+    wireQuery   = mkdefuseQuery("Wires", "Which wires do you need to cut?", [cutRed, cutGreen, cutBlue, cutYellow]);
         // console.log(wireQuery);
-    powerQuery = mkdefuseQuery("Power Source", "Do you need to remove the power source?", ejectPower);
+    powerQuery  = mkdefuseQuery("Power Source", "Do you need to remove the power source?", ejectPower);
         // console.log(powerQuery);
-};
+    phraseQuery = mkdefuseQuery("Pass Phrase", "Which is the correct pass phrase?", passPhrase);
+}
 
 // 4-digit Defusal Code Generator
 function setCode(min, max) {
@@ -132,7 +160,7 @@ function setCode(min, max) {
     } else if (defuseCode > 100 && defuseCode < 1000) {
         defuseCode = "0" + defuseCode;
     };
-};
+}
 
 // Wire Boolean Value Generator
 function setWires() {
@@ -164,7 +192,7 @@ function setWires() {
 
     // Check to make sure it's working!
     // console.log(cutRed, cutGreen, cutBlue, cutYellow);
-};
+}
 
 // Power Source Boolean Value Generator
 function setPower() {
@@ -178,12 +206,12 @@ function setPower() {
 
     // Check to make sure it's working!
     console.log(ejectPower);
-};
+}
 
 // Small Number Generator
 function randomSmallNum() {
     return Math.floor(Math.random() * 2);
-};
+}
 
 // Reset All Answer Variables
 function resetDefusal() {
@@ -195,37 +223,81 @@ function resetDefusal() {
 
     // Set power source
     setPower();
-};
+
+    // Set pass phrase
+    setPhrase();
+
+}
 
 // Set All Defuse Hints
 function setHints() {
     document.getElementById('hintCode').firstElementChild.innerHTML  = codeQuery.items;
     document.getElementById('hintWire').firstElementChild.innerHTML  = wireQuery.items;
     document.getElementById('hintPower').firstElementChild.innerHTML = powerQuery.items;
-};
+}
 
 // Set Current Defusal Question
 function setQuestion(queryObject) {
     document.getElementById('defuseSect').firstElementChild.innerHTML = queryObject.query;
+    
+    if ( queryObject == wireQuery ) {
+        // Red Wire
+        document.getElementById('ans1').firstElementChild.innerHTML = 'Red';
+        document.getElementById('ans1').style.color = '#E50000';
+        // Green Wire
+        document.getElementById('ans2').firstElementChild.innerHTML = 'Green';
+        // Blue Wire
+        document.getElementById('ans3').firstElementChild.innerHTML = 'Blue';
+        document.getElementById('ans3').style.color = '#0BB5FF';
+        // Yellow Wire
+        document.getElementById('ans4').firstElementChild.innerHTML = 'Yellow';
+        document.getElementById('ans4').style.color = '#ffff00';
+    }
+
+    if ( queryObject == codeQuery ) {
+        document.getElementById('ans1').style.color   = '#7ee517';
+        document.getElementById('ans3').style.color   = '#7ee517';
+        document.getElementById('ans4').style.color   = '#7ee517';
+        hideAnswers();
+        document.getElementById('ans1').innerHTML     = '<input type="text" name="code-input" value="Enter Code Here" id="codeInput">';
+        document.getElementById('ans1').style.display = 'block';
+    }
+
+    if ( queryObject == powerQuery ) {
+        document.getElementById('ans1').innerHTML = '<p>yes</p>';
+        document.getElementById('ans2').innerHTML = '<p>no</p>';
+        document.getElementById('ans1').style.display = 'block';
+        document.getElementById('ans2').style.display = 'block';
+    }
+
+    // if (queryObject == phraseQuery) {
+
+    // }
+
 }
 
 
 // Pass Phrase Generator
-/*
-var words  = ["spooky", "tired", "gnarly", "mystery", "brave"];
-var words2 = ["ghost", "archer", "mister", "monkey", "sick"];
-var words3 = ["devil", "arrow", "explosion", "proceed", "sleeps"];
 
-var getRandomPhrase = function () {
-    return words[Math.floor(Math.random() * words.length)] + " " + words2[Math.floor(Math.random() * words2.length)] + " " + words3[Math.floor(Math.random() * words3.length)];
-};
+// var words  = ["spooky", "tired", "gnarly", "mystery", "brave"];
+// var words2 = ["ghost", "archer", "mister", "monkey", "sick"];
+// var words3 = ["devil", "arrow", "explosion", "proceed", "sleeps"];
+function setPhrase() {
+    var getRandomPhrase = function () {
+        word1 = randomizeWord(words);
+        word2 = randomizeWord(words2);
+        word3 = randomizeWord(words3);
+        return word1 + " " + word2 + " " + word3;
+    };
 
-var phrase = getRandomPhrase();
+    phrase = getRandomPhrase();
 
-console.log(phrase);
+    function randomizeWord(inputWords) {
+        return inputWords[Math.floor(Math.random() * inputWords.length)];
+    };
 
-document.getElementById("phrase").textContent = phrase;
-*/
+    console.log(passPhrase);
+}
 
 // Defunct Parts And Things That Worked But Weren't What I Needed //
 /*
